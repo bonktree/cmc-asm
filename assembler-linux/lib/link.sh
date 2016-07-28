81007f3c8d515928560cf7b2b7f04e3a5beb82a6
porrided <ar@cs.msu.ru>
2016-07-28 14:48:36 +0300

    Some backend support for multi-stage execution

--- assembler-linux/lib/link.sh
+++ assembler-linux/lib/link.sh
@@ -8,14 +8,8 @@
 [ -z $FN ] && FN="$1"
 [ -z $FN ] && exit 40
 
-# Cleaning binaries
-if [ -z $MASM_ACTION ]; then
-    [ -f $FN.exe ] && rm $FN.exe
-    [ -f $FN.obj ] && rm $FN.obj
-fi
-
-ML="c:\\masm4\\masm.exe"
-ML_CL="$ML /c /Ic:\\ $FN,$FN,$FN;"
+LINKER="c:\\masm4\\link.exe"
+LINKER_CL="$LINKER $FN+c:\\ioproc.obj,$FN;"
 
 sh -c "dosbox -conf ""\"$TMP_CONF\""" \
     -c '""u:\\""' \
@@ -24,12 +18,12 @@ sh -c "dosbox -conf ""\"$TMP_CONF\""" \
     ""$DOSBOX_OUTPUT_REDIRECTION"
 
 find . -iname "$FN"'*' -print | $MASM_PATH/lower.sh
-if [ ! -f $FN.obj ] ; then
-    echo "Compilation error."
+if [ ! -f $FN.exe ] ; then
+    echo "Linking error."
     rm -f $TMP_CONF
-    exit 10
+    exit 11
 else
-    echo "Compilation complete."
+    echo "Linking complete."
 fi
 
 
